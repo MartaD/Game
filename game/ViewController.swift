61a94f04544a5619953284f931105f2261628056
Marta Wozniak <M.Wozniak@kainos.com>
2015-07-24 16:15:48 +0200

    Initial Commit

---
+++ game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  game
+//
+//  Created by Marta Wozniak on 24/07/15.
+//  Copyright (c) 2015 Marta Wozniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
